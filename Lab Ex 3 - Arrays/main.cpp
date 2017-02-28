e970b7254305719b4f19e6333a2b36090bc1665d
Ryan Appel <rdappel@gmail.com>
2017-02-27 22:31:59 -0600

    initial commit

---
+++ Lab Ex 3 - Arrays/main.cpp
@@ -0,0 +1,27 @@
+
+// Lab Exercise 3 - Arrays
+// Instructions:
+/*
+	[ ] Write a program that performs the following:
+		[ ] Prompt the user to input five numbers.
+		[ ] Store the input into either an array or a vector.
+		[ ] Once the user is finished entering numbers:
+			[ ] Display the five numbers in reverse order.
+			[ ] Display the highest number entered.
+			[ ] Display the lowest number entered.
+		[ ] Extra Credit: Allow the user to enter as many numbers as they want(at least 5).
+	[ ] Compress(zip) your program and submit it to Blackboard.
+*/
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+
+
+	_getch();
+	return 0;
+}
