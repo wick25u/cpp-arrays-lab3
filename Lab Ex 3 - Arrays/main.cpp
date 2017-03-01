052809ef62b23acf175324756506ef2817075532
Bowman <500150873@fvtc.edu>
2017-02-28 18:29:15 -0600

    Yes

--- Lab Ex 3 - Arrays/main.cpp
+++ Lab Ex 3 - Arrays/main.cpp
@@ -2,30 +2,48 @@
 // Lab Exercise 3 - Arrays
 // Instructions:
 /*
-	[ ] Write a program that performs the following:
-		[ ] Prompt the user to input five numbers.
-		[ ] Store the input into either an array or a vector.
-		[x] Once the user is finished entering numbers:
-			[x] Display the five numbers in reverse order.
-			[x] Display the highest number entered.
-			[x] Display the lowest number entered.
-		[ ] Extra Credit: Allow the user to enter as many numbers as they want(at least 5).
-	[ ] Compress(zip) your program and submit it to Blackboard.
+	[  ] Write a program that performs the following:
+		[ x ] Prompt the user to input five numbers.
+		[ x ] Store the input into either an array or a vector.
+		[  ] Once the user is finished entering numbers:
+			[  ] Display the five numbers in reverse order.
+			[  ] Display the highest number entered.
+			[  ] Display the lowest number entered.
+		[  ] Extra Credit: Allow the user to enter as many numbers as they want(at least 5).
+	[  ] Compress(zip) your program and submit it to Blackboard.
 */
 
+
+
+//Rick Bowman, Alex Wick, Brian Harrah
 #include <iostream>
 #include <conio.h>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-	const int SIZE = 5;
-	int arr[SIZE];
+	int size;
+	std::cout << "How many numbers would you like to input? ";
+	std::cin >> size;
 
-	for (int i = 0; i < SIZE; i++)
+	std::vector<int> vec;
+
+	std::vector<int>::iterator it;
+	for (int it = 0; it < size; it++)
 	{
+		int temp;
+		std::cout << "Please enter a number: ";
+		std::cin >> temp;
+		vec.push_back(temp);
+	}
 
+	std::vector<int>::reverse_iterator rit;
+	for ()
+	{
+		std::cout <<
 	}
 
 
